3a90842eefaa5d8abeb52ba99a40ac4656f0295f
QUICKLyas <2298930148@qq.com>
2023-04-08 04:49:54 +0800

    v0.1.2304080430 check OK 4 Taobao

--- src/taobao_check.py
+++ src/taobao_check.py
@@ -22,15 +22,18 @@ options.add_argument('--user-data-dir='+p)
 options.add_argument('--disable-blink-features=AutomationControlled')
 # 浏览器打开
 driver = webdriver.Chrome(options=options)
+flag = True
 
 for index in range(1, 101):
     # 检测数据是否已经存储
     goods_list = fu.readJson(path="./data/taobao/",
                              file_name="taobao"+str(index))
     if len(goods_list) != 0:
-        print("next page" + str(index))
+
+        print("next page" + str(index), "size: " + str(len(goods_list)))
         continue
     # 重新获取数据
+    flag = False
     driver.get('https://s.taobao.com/search?q=%E5%A4%A7%E7%B1%B3&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20230407&ie=utf8&bcoffset=1&ntoffset=1&p4ppushleft=2%2C48&s='+str(0+44*index))
     i = 1
     while(True):
@@ -51,20 +54,26 @@ for index in range(1, 101):
             location = driver.find_element(
                 By.CSS_SELECTOR, '#mainsrp-itemlist > div > div > div:nth-child(1) > div:nth-child('+str(i)+') > div.ctx-box.J_MouseEneterLeave.J_IconMoreNew > div.row.row-3.g-clearfix > div.location').text
             # mainsrp-itemlist > div > div > div:nth-child(1) > div:nth-child(1) > div.ctx-box.J_MouseEneterLeave.J_IconMoreNew > div.row.row-3.g-clearfix > div.location
-            print(title, price, sales, location, shop_name, detail_url)
-            item_dict = {
-                "title": title,
-                "price": price,
-                "sales": sales,
-                "location": location,
-                "shop_name": shop_name,
-                "detail_url": shop_name
-            }
-            goods_list.append(item_dict)
-            i += 1
+            # print(title, price, sales, location, shop_name, detail_url)
+
         except:
+            print("error")
             break
+        item_dict = {
+            "title": title,
+            "price": price,
+            "sales": sales,
+            "location": location,
+            "shop_name": shop_name,
+            "detail_url": shop_name
+        }
+        goods_list.append(item_dict)
+        i += 1
 
     fu.writeJson(goods_list, path="./data/taobao/",
                  file_name="taobao"+str(index))
     time.sleep(10)
+if(flag == True):
+    print("OK")
+else:
+    print("have more try")
